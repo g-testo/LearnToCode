a3d2946075b3422b94150025e81231e0e7b18a85
Gregory Testo <gt@gregorytesto.com>
2023-04-28 13:39:06 -0400

    added missing parts of show cart

--- Workbook3/WorkshopThree_OnlineStore/src/main/java/com/gt/Main.java
+++ Workbook3/WorkshopThree_OnlineStore/src/main/java/com/gt/Main.java
@@ -52,30 +52,48 @@ public class Main {
             // Handle catch/exception
 
     // Create static method called showProducts that return nothing
+        // iterate through the inventory
+            // Print each product one at a time
+
+        // Initialize variable for productIdInput
+        // Create do-while loop that runs unless the user enters "x"
+            // Prompt user for productIdInput or let them know that they can exit with "x"
+            // Iterate through inventory
+                // if there is a product that has a matching productIdInput
+                    // add product to cart
+                    // break
 
     // Create static method called showCart that return nothing
-        // Create sub-menu
+        // Display total number of items in the cart
+        // Iterate through the cart
+            // Print each item in cart one at a time
+
+    // Create sub-menu
             // Initialize subInput variable for user input
             // Show prompt for user showing the possible options and asking for an input
-            // Create do-while loop that runs unless the user enters "2"
+            // Create do-while loop that runs unless the user enters "x"
                 // Create switch statement that handles cases related to each menu item
-                    // Case "1": Checkout
+                    // Case "c": Checkout
                         // Runs custom static method called checkout
-                    // Case "2": Go Back To Home Screen
+                    // Case "x": Go Back To Home Screen
                         // Print message that says "Going back to home screen"
                     // Default:
                         // Show message "Input command not found"
+
+    // Create static method called checkout that return nothing
+
 }
+
 
 // Create product class
 
-// Initialize product properties:
-// int id
-// String name
-// float price
+    // Initialize product properties:
+    // int id
+    // String name
+    // float price
 
-// Create constructor that takes in all properties
+    // Create constructor that takes in all properties
 
-// Create getters and setters
+    // Create getters and setters
 
-// (Optional) Create overridden toString method
+    // (Optional) Create overridden toString method
