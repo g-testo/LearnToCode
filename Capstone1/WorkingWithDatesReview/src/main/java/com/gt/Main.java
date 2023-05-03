feaf9d44b90ae4a77897d9b59c68d79e0abb7a95
Gregory Testo <gt@gregorytesto.com>
2023-05-03 12:22:53 -0400

    added a review of Dates from capstone week

---
+++ Capstone1/WorkingWithDatesReview/src/main/java/com/gt/Main.java
@@ -0,0 +1,83 @@
+package com.gt;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+//        // Converting from a string to a date ///////////
+//        String date1 = "2000-12-01";
+//        // 1: Create a pattern for the date
+//        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//        // 2: Parse/convert the date using the created pattern
+//        LocalDate formattedDate1 = LocalDate.parse(date1, dateTimeFormatter1);
+////        Print to confirm
+////        System.out.println(formattedDate1.getYear());
+//
+//        // Converting from a date to a formatted date string
+//        // 1: Create an output date format
+//        DateTimeFormatter dateTimeFormatterOutput1 = DateTimeFormatter.ofPattern("yy-MM-dd");
+//        // 2: Parse/convert the date object to the output pattern
+//        String outputDate1 = formattedDate1.format(dateTimeFormatterOutput1);
+//        System.out.println(outputDate1);
+//
+//        String date2 = "12-01-00";
+//        // 1: Create a pattern for the date
+//        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofPattern("MM-dd-yy");
+//        // 2: Parse/convert the date using the created pattern
+//        LocalDate formattedDate2 = LocalDate.parse(date2, dateTimeFormatter2);
+////      System.out.println(formattedDate2.getYear());
+//
+//        String date3 = "01-12-2020";
+//        // 1: Create a pattern for the date
+//        DateTimeFormatter dateTimeFormatter3 = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+//        // 2: Parse/convert the date using the created pattern
+//        LocalDate formattedDate3 = LocalDate.parse(date3, dateTimeFormatter3);
+////        System.out.println(formattedDate3.getYear());
+
+
+
+
+        // Capstone Example ///////////////////////
+        
+
+
+//        String date = "2023-04-15";
+//        String time = "10:13:25";
+//        String dateTime = date + " " + time; // "2023-04-15 10:13:25"
+//        // 1: Create a pattern for the date
+//        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//
+////        2: Parse/convert the date using the created pattern
+//        LocalDateTime localDateTime = LocalDateTime.parse(dateTime, dateTimeFormatter);
+//
+//        System.out.println(localDateTime);
+
+
+//        .compare
+
+//        1) Month To Date
+//        BeginningOfMonth Create a date LocalDate.now().getYear() + "-" LocalDate.now().getMonth() + "-" 00 + 00:00:00"
+//        givenDateTime > BeginningOfMonth
+
+//        2) Previous Month
+
+//        prevMonth = LocalDate.now().getYear() + "-" LocalDate.now().minusMonths(1).getMonth() + "-" 00 + 00:00:00"
+//        currentMonth = LocalDate.now().getYear() + "-" LocalDate.now().getMonth() + "-" 00 + 00:00:00"
+
+//        givenDateTime > prevMonth && givenDateTime < currentMonth
+
+//        3) Year To Date
+            // From the beginning of the year
+            // beginningYear = LocalDate.now().getYear() + "-01-01 00:00:00"
+            // givenDateTime > beginningYear
+//        4) Previous Year
+        // All transactions from last year
+        // beginningOfLastYear = LocalDate.now().minusYears(1).getYears() + "-01-01 00:00:00"
+        // beginningOfCurrentYear = LocalDate.now().getYear() + "-01-01 00:00:00"
+
+        // givenDateTime > beginningOfLastYear && givenDateTime < beginningOfCurrentYear
+
+    }
+}
--- Workbook3/WorkshopThree_OnlineStore/src/main/java/com/gt/Main.java
+++ Workbook3/WorkshopThree_OnlineStore/src/main/java/com/gt/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Run custom static method called loadProductsFromFile
+        loadProductsFromFile();
 
         // Create main menu:
             // Initialize variable for user input
@@ -21,8 +22,10 @@ public class Main {
             // Create do-while loop that runs unless the user enters "3"
                 // Create switch statement that handles cases related to each menu item
                     // Case "1": Show Products
+                        showProducts();
                         // Runs custom static method called showProducts
                     // Case "2": Show Cart
+                        showCart();
                         // Runs custom static method called showCart
                     // Case "3": Exit
                         // Print message that says "Exiting"
@@ -31,72 +34,80 @@ public class Main {
 
     }
 
-    // Create static method called loadProductsFromFile that return nothing
-        // Read data from file: From WorkingWithFileIO >> BufferedReaderExercise example
-            // Create a try/catch statement to handle file import
-                // Create an instance of FileReader with an approximate path of "./src/main/java/com/<YourInitial>/inventory.csv"
-                // Create an instance of BufferedReader that takes in the FileInputStream instance above
+    public static void loadProductsFromFile(){
+        // Create static method called loadProductsFromFile that return nothing
+            // Read data from file: From WorkingWithFileIO >> BufferedReaderExercise example
+                // Create a try/catch statement to handle file import
+                    // Create an instance of FileReader with an approximate path of "./src/main/java/com/<YourInitial>/inventory.csv"
+                    // Create an instance of BufferedReader that takes in the FileInputStream instance above
 
-                // Create a while loop that iterates through each line of the file
-                    // Parse/Split the line on "|" and store in variable: Hint .split("\\|");
+                    // Create a while loop that iterates through each line of the file
+                        // Parse/Split the line on "|" and store in variable: Hint .split("\\|");
 
-                    // Create variables that stores each property
-                        // String id
-                        // String name
-                        // float price
+                        // Create variables that stores each property
+                            // String id
+                            // String name
+                            // float price
 
-                    // Create a variable that stores a Product instance using the data from the current line
+                        // Create a variable that stores a Product instance using the data from the current line
 
-                    // Add the variable to the static inventory variable
-                // Close the scanner instance
-            // Handle catch/exception
+                        // Add the variable to the static inventory variable
+                    // Close the scanner instance
+                // Handle catch/exception
+    }
 
-    // Create static method called showProducts that return nothing
-        // iterate through the inventory
-            // Print each product one at a time
+    public static void showProducts(){
+        // Create static method called showProducts that return nothing
+            // iterate through the inventory
+                // Print each product one at a time
 
-        // Initialize variable for productIdInput
-        // Create do-while loop that runs unless the user enters "x"
-            // Prompt user for productIdInput or let them know that they can exit with "x"
-            // Iterate through inventory
-                // if there is a product that has a matching productIdInput
-                    // add product to cart
-                    // break
-
-    // Create static method called showCart that return nothing
-        // Display total number of items in the cart
-        // Iterate through the cart
-            // Print each item in cart one at a time
-
-        // Create sub-menu
-            // Initialize subInput variable for user input
-            // Show prompt for user showing the possible options and asking for an input
+            // Initialize variable for productIdInput
             // Create do-while loop that runs unless the user enters "x"
-                // Create switch statement that handles cases related to each menu item
-                    // Case "c": Checkout
-                        // Runs custom static method called checkout
-                    // Case "x": Go Back To Home Screen
-                        // Print message that says "Going back to home screen"
-                    // Default:
-                        // Show message "Input command not found"
+                // Prompt user for productIdInput or let them know that they can exit with "x"
+                // Iterate through inventory
+                    // if there is a product that has a matching productIdInput
+                        // add product to cart
+                        // break
+    }
 
-    // Create static method called checkout that return nothing
-        // Create a variable to store the sum of the cart
-        // Iterate through the cart
-            // Add each product price to the sum variable
-        // Print out the total owed(sum variable) by the user
+    public static void showCart(){
+        // Create static method called showCart that return nothing
+            // Display total number of items in the cart
+            // Iterate through the cart
+                // Print each item in cart one at a time
 
-        // Print a message to the user to pay for the items
-        // Create a variable and prompt from the user for an int that represents how much they are willing to pay in cash
+            // Create sub-menu
+                // Initialize subInput variable for user input
+                // Show prompt for user showing the possible options and asking for an input
+                // Create do-while loop that runs unless the user enters "x"
+                    // Create switch statement that handles cases related to each menu item
+                        // Case "c": Checkout
+                            handleCheckout();
+                            // Runs custom static method called handleCheckout
+                        // Case "x": Go Back To Home Screen
+                            // Print message that says "Going back to home screen"
+                        // Default:
+                            // Show message "Input command not found"
+    }
 
-        // If the amount given by the user covers the entire amount owed
-            // Print out the difference that is left over
-            // Print all the items that the user has purchased
-            // Clear the cart arraylist
-            // Return to the main menu
-        // Otherwise
-            // Let the user know that the amount given doesn't cover the full amount
+    public static void handleCheckout(){
+        // Create static method called handleCheckout that return nothing
+            // Create a variable to store the sum of the cart
+            // Iterate through the cart
+                // Add each product price to the sum variable
+            // Print out the total owed(sum variable) by the user
 
+            // Print a message to the user to pay for the items
+            // Create a variable and prompt from the user for an int that represents how much they are willing to pay in cash
+
+            // If the amount given by the user covers the entire amount owed
+                // Print out the difference that is left over
+                // Print all the items that the user has purchased
+                // Clear the cart arraylist
+                // Return to the main menu
+            // Otherwise
+                // Let the user know that the amount given doesn't cover the full amount
+    }
 }
 
 
