1c85baed0be63070dc669700d32e278f8bb7384b
Gregory Testo <gt@gregorytesto.com>
2023-05-08 16:01:08 -0400

    finished Area or Perimeter CW problem

--- CodeWarsPractice/src/main/java/com/gt/AreaOrPerimeter.java
+++ CodeWarsPractice/src/main/java/com/gt/AreaOrPerimeter.java
@@ -7,10 +7,56 @@ package com.gt;
 //2nd is height
 //3rd is a calculation
 
+// Step 1
+// Inputs: int width, int height and int givenCalculation
+// Output: String Is whether the givenCalculation is the "area", "perimeter" or "neither"
 
+// Inputs: 2, 5, 12
+// Output: "Neither"
+
+// 8, 26, 208
+// Output: "Area"
+
+// 7, 9, 32
+// Output: "Perimeter"
+
+// Step 2
+
+// Inputs: int width, int height and int givenCalculation
+
+// create variables that calculate both area and perimeter
+    // area: width * height
+    // perimeter: (width+height)*2 - Pemdas
+
+// if area is equals givenCalculation then return "Area"
+// Otherwise if perimeter is equal to givenCalculation then return "Perimeter"
+// Otherwise return "Neither"
+
+// Step 3
 public class AreaOrPerimeter {
     public static void main(String[] args) {
-
+        String returnValue = calcIfAreaPerimeter(2, 5, 14);
     }
 
+    public static String calcIfAreaPerimeter(int width, int height, int givenCalculation){
+        int area = width * height;
+        int perimeter = (width + height)*2;
+
+        if(givenCalculation == area){
+            return "Area";
+        } else if(givenCalculation == perimeter){
+            return "Perimeter";
+        } else {
+            return "Neither";
+        }
+    }
 }
+
+// Step 4
+// Can we make it better?
+    // Efficiency
+    // Readability
+    // Maintainability
+    // Use better conventions
+        // Naming
+        // DRY, Don't repeat yourself
