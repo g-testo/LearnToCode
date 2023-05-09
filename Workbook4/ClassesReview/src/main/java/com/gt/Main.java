db05ee9608183456d73605e6c998f6c3c1e7e14c
Gregory Testo <gt@gregorytesto.com>
2023-05-09 09:25:52 -0400

    added classes review project

---
+++ Workbook4/ClassesReview/src/main/java/com/gt/Main.java
@@ -0,0 +1,17 @@
+package com.gt;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+// Create a User class >> name, username, email
+// >> Generator <<
+// Two constructors: one that accepts no data, another that accepts all data
+// Getters and setters for each property
+// Override toString
+
+// if complete:
+// >> (Add two fields)
+// Create two instances
