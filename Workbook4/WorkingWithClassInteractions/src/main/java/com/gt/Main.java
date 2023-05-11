2205110f348ae733f32dbc3a70b863deeef4d40b
Gregory Testo <gt@gregorytesto.com>
2023-05-11 13:55:56 -0400

    added Hotel example

--- Workbook4/WorkingWithClassInteractions/src/main/java/com/gt/Main.java
+++ Workbook4/WorkingWithClassInteractions/src/main/java/com/gt/Main.java
@@ -1,7 +1,103 @@
 package com.gt;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Hotel
+        // Reservation
+        // Guest
+        // Room
+
+        Hotel hotel1 = new Hotel(1);
+        Reservation reservation1 = new Reservation(1);
+        Guest guest1 = new Guest(1);
+        Room room1 = new Room(1);
+    }
+}
+
+class Hotel{
+    private int id;
+    public Hotel(int id){
+        this.id = id;
+    }
+    private ArrayList<Reservation> reservations;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(ArrayList<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+}
+class Reservation{
+    private int id;
+    private Guest owner;
+    private Room room;
+
+    public Reservation(int id){
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Guest getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Guest owner) {
+        this.owner = owner;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+}
+class Guest{
+    private int id;
+    public Guest(int id){
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+class Room{
+    private int id;
+    public Room(int id){
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
