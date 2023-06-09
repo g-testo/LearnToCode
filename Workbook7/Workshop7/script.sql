13a6da3ade7229f3a79b7f29184aaba6ae7a5aa6
Gregory Testo <gt@gregorytesto.com>
2023-06-09 11:40:05 -0400

    added scripts work workshop 7

--- Workbook7/Workshop7/script.sql
+++ Workbook7/Workshop7/script.sql
@@ -1,3 +1,29 @@
+DROP TABLE IF EXISTS hotels;
+DROP TABLE IF EXISTS floors;
+DROP TABLE IF EXISTS rooms;
+
+CREATE TABLE hotels(
+    id int NOT NULL,
+    name varchar(255),
+    totalFloors int,
+    totalOccupancy int,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE floors(
+	id int PRIMARY KEY,
+    number int,
+    hotelId int,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE rooms(
+	id int NOT NULL,
+    number int,
+    floorId int,
+    PRIMARY KEY(id)
+);
+
 
 INSERT INTO hotels VALUES(1, 'Gregs Gregarious Games and Fun', 23, 1000);
 INSERT INTO floors VALUES(1,1,1);
