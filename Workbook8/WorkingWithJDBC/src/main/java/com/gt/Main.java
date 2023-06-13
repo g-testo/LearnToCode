3c422e65c2d76e0864b94f43f8e9c724e0acfc98
Gregory Testo <gt@gregorytesto.com>
2023-06-13 14:57:44 -0400

    added try with resources

--- Workbook8/WorkingWithJDBC/src/main/java/com/gt/Main.java
+++ Workbook8/WorkingWithJDBC/src/main/java/com/gt/Main.java
@@ -7,39 +7,36 @@ import java.sql.*;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet results = null;
-        try {
-            String username = args[0];
-            String password = args[1];
+        String username = args[0];
+        String password = args[1];
+        try(
+                Connection connection = DriverManager
+                        .getConnection("jdbc:mysql://" +
+                                username + ":" + password +
+                                "@us-cdbr-east-06.cleardb.net/heroku_4fe1d93c00fe86e?reconnect=true"
+                        );
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "SELECT * FROM hotels WHERE name LIKE ?"
+                );
+        ) {
 
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            connection = DriverManager.getConnection("jdbc:mysql://" + username + ":" + password + "@us-cdbr-east-06.cleardb.net/heroku_4fe1d93c00fe86e?reconnect=true");
-
-            preparedStatement = connection.prepareStatement(
-                    "SELECT * FROM hotels WHERE name LIKE ?"
-            );
-
             preparedStatement.setString(1, "St%");
 
-            results = preparedStatement.executeQuery("jkdsjfklsdjkj");
-
-            while (results.next()) {
-                String hotel = results.getString("name");
-                int totalFloors = results.getInt("totalFloors");
-                System.out.println(totalFloors);
-                System.out.println(hotel);
+            try(ResultSet results = preparedStatement.executeQuery()){
+                while (results.next()) {
+                    String hotel = results.getString("name");
+                    int totalFloors = results.getInt("totalFloors");
+                    System.out.println(totalFloors);
+                    System.out.println(hotel);
+                }
             }
+
         } catch (SQLException | ClassNotFoundException e){
             e.printStackTrace();
-        } finally {
-            if (results != null) results.close();
-            if (preparedStatement != null) preparedStatement.close();
-            if (connection != null) connection.close();
         }
 
     }
