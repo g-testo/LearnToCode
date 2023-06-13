51b06e02b69e207069246e6d1124419d8162f85b
Gregory Testo <gt@gregorytesto.com>
2023-06-13 11:05:25 -0400

    created simple example of db connection

--- Workbook8/WorkingWithJDBC/src/main/java/com/gt/Main.java
+++ Workbook8/WorkingWithJDBC/src/main/java/com/gt/Main.java
@@ -9,10 +9,22 @@ public class Main {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/world");
 
+            Connection connection = DriverManager.getConnection("dbc:mysql://ba4aff20914505:6884f4ee@us-cdbr-east-06.cleardb.net/heroku_4fe1d93c00fe86e?reconnect=true");
+            Statement statement = connection.createStatement();
 
-        } catch (ClassNotFoundException e) {
+            String query = "SELECT * FROM hotels;";
+
+            ResultSet results = statement.executeQuery(query);
+
+            while(results.next()){
+                String hotel = results.getString("name");
+                System.out.println(hotel);
+            }
+
+            connection.close();
+
+        } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
 
